1db8a37901847302bb50ad7b7b6cdb9ba39f0731
Euael Eshete <euaelesh@outlook.com>
2021-12-25 13:47:46 +0300

    December 25, 2021 01:47 PM

--- memokeria/Program.cs
+++ memokeria/Program.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 namespace memokeria
 {
@@ -10,15 +11,28 @@ namespace memokeria
     {
         public static void Main(string[] args)
         {
-            Solution a = new Solution();
+            // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            // int rankedCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            // List<int> ranked = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(rankedTemp => Convert.ToInt32(rankedTemp)).ToList();
+
+            // int playerCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            // List<int> player = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(playerTemp => Convert.ToInt32(playerTemp)).ToList();
+            
+            // Solution a = new Solution();
             // int[] arr = new[] {5, 4, -1, 7, 8};
             // Console.WriteLine(a.MaxSubArray(arr));
             int[] arr1 = new[] {100, 90, 90, 80, 75, 60};
             int[] arr2 = new[] {50, 65, 77, 90, 102};
+            int[] arr3 = new[] {5, 1, 2, 2, 3, 4, 5};
             // Solution.printColl(a.compareTriplets(arr1.ToList(), arr2.ToList()));
-            Solution.ClimbingLeaderboard(arr1.ToList(), arr2.ToList());
-
             
+            // Solution.ClimbingLeaderboard(arr1.ToList(), arr2.ToList());
+            var x =Solution.countingSort(arr3.ToList());
+            foreach (var va in x)
+                Console.Write($" {va} ");
         }
     }
 }
--- memokeria/Solution.cs
+++ memokeria/Solution.cs
@@ -6,14 +6,16 @@ using System.Linq;
 namespace memokeria
 {
     class Solution {
-        public static void printColl<T>(List<T> x)
+        
+        public static void PrintColl<T>(List<T> x) // prints any collections
         {
-            foreach (var Va in x)
+            foreach (var va in x)
             {
-                Console.Write($" {Va} ");
+                Console.Write($" {va} ");
             }
             Console.WriteLine(" ");
         }
+        
         public string DecodeString(string s) // works
         {
             string x = "";
@@ -136,22 +138,22 @@ namespace memokeria
         public int Reverse(int x) // works well
         {
             int retr;
-            string RE = x.ToString();
+            string re = x.ToString();
             bool isNeg = false;
             string tobeReturned = "";
-            if (RE.StartsWith("-"))
+            if (re.StartsWith("-"))
                 isNeg = true;
             if (isNeg)
             {
-                for (int i = RE.Length - 1; i >= 1; i--)
-                    tobeReturned += RE[i];
+                for (int i = re.Length - 1; i >= 1; i--)
+                    tobeReturned += re[i];
                 Int32.TryParse(tobeReturned, out retr);
                 return retr * -1;
             }
             else
             {
-                for (int i = RE.Length - 1; i >= 0; i--)
-                    tobeReturned += RE[i];
+                for (int i = re.Length - 1; i >= 0; i--)
+                    tobeReturned += re[i];
                 Int32.TryParse(tobeReturned, out retr);
                 return retr;
             }
@@ -197,10 +199,10 @@ namespace memokeria
             return points;
         }
         
-        public static List<int> ClimbingLeaderboard(List<int> ranked, List<int> player) // Time Limit exceeded
+        public static List<int> ClimbingLeaderboard(List<int> ranked, List<int> player) // Wrong Answer
         {
-            List<int> returnRank = new List<int>();
-            List<int> prevRank = new List<int>();
+            var returnRank = new List<int>();
+            var prevRank = new List<int>();
             prevRank.Add(1);
             int rankCounter = 1;
             for (int i = 1; i < ranked.Count; i++)
@@ -212,31 +214,168 @@ namespace memokeria
             }
             int playerCount = player.Count;
             // to make the loop faster, used a reversed for loop
+            int iter = 0;
             for (var index = player.Count - 1; index >= 0; index--)
             {
-                var VARIABLE = player[index];
-                for (int i = 0; i < ranked.Count; i++)
-                    if (VARIABLE >= ranked[i])
+                var va = player[index];
+                for (; iter < ranked.Count; iter++)
+                    if (va >= ranked[iter])
                     {
-                        returnRank.Add(prevRank[i]);
+                        returnRank.Add(prevRank[iter]);
                         playerCount--;
                         break;
                     }
             }
-
+            // i think this is the problem
             while (playerCount > 0)
             {
-                int itemp = player.Count - playerCount;
-                if (player[itemp] == player[itemp - 1])
+                int itemp = playerCount - 1;
+                if (itemp == 0)
+                    returnRank.Add(rankCounter);
+                else if (player[itemp] == player[itemp - 1])
                     returnRank.Add(rankCounter);
                 else 
                     returnRank.Add(++rankCounter);
                 playerCount--;
             }
+            // returnRank.reverse();
+            List<int> reReturnRank = new List<int>();
+            for (int i = returnRank.Count - 1; i >= 0; i--)
+            {
+                reReturnRank.Add(returnRank[i]);
+            }
+            PrintColl(reReturnRank);
+            return reReturnRank;
+        }
+        
+        public int[] TopKFrequent(int[] nums, int k) // wrong answer
+        {
+            var retu = new int[k];
+            
+            var freq = new SortedDictionary<int, int> {{nums[0], 1}}; // value, frequency
+            for(var i = 1; i < nums.Length - 1; i++){
+                if(nums[i] == nums[i - 1])
+                    freq[nums[i]]++;
+                else
+                    freq.Add(nums[i], 1);
+            }
 
-            returnRank.Reverse();
-            printColl(returnRank);
-            return returnRank;
+            var frequency = freq.Values.ToArray();
+            while (k > 0)
+            {
+                retu[k] = freq.Last().Key;
+                freq.Remove(frequency[k]);
+                k--;
+            }
+
+            return retu;
+        }
+        
+        public static List<int> gradingStudents(List<int> grades) // works well
+        {
+            var ret = new List<int>();
+            foreach (var va in grades)
+            {
+                if (va < 38)
+                    ret.Add(va);
+                else
+                {
+                    int temp = 5 - (va % 5);
+                    if (temp >= 3)
+                        ret.Add(va);
+                    else
+                        ret.Add(va + temp);
+                }
+            }
+            return ret;
+        }
+        
+        public static void countSwaps(List<int> a) // works well
+        {
+            int swapCount = 0;
+            for (int i = 0; i < a.Count; i++)
+            {
+                for (int j = i + 1; j < a.Count; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        (a[i], a[j]) = (a[j], a[i]); // deconstructor swap
+                        // int temp = a[i];
+                        // a[i] = a[j];
+                        // a[j] = temp;
+                        swapCount++;
+                    }
+                }
+            }
+            Console.WriteLine($"Array is sorted in {swapCount} swaps.");
+            Console.WriteLine($"First Element: {a[0]}");
+            Console.WriteLine($"Last Element: {a.Last()}");
+            
+        }
+        
+        public static int countingValleys(int steps, string path) // done easily
+        {
+            int valley = 0;
+            int ic = 0;
+            foreach (var v in path)
+            {
+                if (v == 'U')
+                {
+                    ic += 1;
+                    if (ic == 0)
+                        valley += 1;
+                }else if (v == 'D')
+                    ic -= 1;
+            }
+            return valley;
+        }
+        
+        public static void insertionSort1(int n, List<int> arr) // works
+        {
+            int x = arr.Last();
+            for (var i = arr.Count - 2; i >= 0; i--)
+            {
+                if (arr[i] < x)
+                {
+                    arr[i + 1] = x;
+                    foreach (var va in arr)
+                        Console.Write($" {va}");
+                }
+                else if (i == 0 && arr[i] >= x)
+                {
+                    arr[i + 1] = arr[i];
+                    foreach (var va in arr)
+                        Console.Write($" {va}");
+                    Console.WriteLine();
+                    arr[i] = x;
+                    foreach (var va in arr)
+                        Console.Write($" {va}");
+                }
+                else
+                    arr[i + 1] = arr[i];
+
+                foreach (var va in arr)
+                    Console.Write($" {va}");
+                Console.WriteLine();
+            }
+        }
+        
+        public static List<int> countingSort(List<int> arr) // works well: save for further improvement
+        {
+            int max = arr.Max();
+            List<int> freq = new List<int>();
+            for (int i = 0; i < max + 1; i++)
+                freq.Add(0);
+            
+            // int[] freq = new int[max + 1];
+            foreach (var t in arr)
+                freq[t] += 1;
+            // var ret = new List<int>();// returns the sorrtrr
+            // for (int i = 0; i < freq.Length; i++)
+            //     for (int j = 0; j < freq[i]; j++)
+            //             ret.Add(i);
+
+            return freq;
         }
     }
 }
